08ad8be7fbe7417119709f45ac98dd34b257929e
djharshit <harshitmeh111@gmail.com>
2022-02-01 13:14:59 +0530

    2 - wrapper class

---
+++ WrapperClass.java
@@ -0,0 +1,11 @@
+// Sum of numbers using wrapper class
+
+public class WrapperClass
+{
+    public static void main(String[] args) {
+        Integer a = 10;
+        Integer b = 20;
+        System.out.println("a: "+a+" b: "+b);
+        System.out.println("Sum: "+Integer.sum(a,b));
+    }
+}
